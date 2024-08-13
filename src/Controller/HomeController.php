fb3e16d3ec77c865fb57808173ade1662494f5eb
annabreyer <breyer.anna@gmail.com>
2024-08-13 13:33:53 +0200

    change security for index page, so it can be seen without being logged in

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\PriceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +12,14 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(PriceRepository $priceRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $voucherPrices = $priceRepository->findActiveVoucherPrices();
+        $unitaryPrice = $priceRepository->findOneBy(['isUnitary' => true]);
+
+        return $this->render('home/index.html.twig', [
+            'voucherPrices' => $voucherPrices,
+            'unitaryPrice' => $unitaryPrice,
+        ]);
     }
 }
